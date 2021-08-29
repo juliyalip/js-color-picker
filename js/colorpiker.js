3b9e8bae557dc28e0895992325138a6792d66d4e
Juliya Lipatova <575@ukr.net>
2021-08-29 13:22:22 +0300

    add colorPicker

---
+++ js/colorpiker.js
@@ -0,0 +1,55 @@
+import colors from './colors.js';
+
+console.log(colors)
+
+
+const container = document.querySelector('.colorList');
+
+
+
+
+const createColorItem = ({rgb, label}) => {
+    return `<li class="colorItem"> <div class="btn"  data-color="${rgb}" style="background-color: ${rgb}"> ${label}
+    </div> </li>`
+};
+
+const createColorListMarkup = colors.map(createColorItem).join('');
+
+container.innerHTML = createColorListMarkup;
+
+container.addEventListener('click', onClickBtn)
+
+function onClickBtn(e) {
+    if (!e.target.classList.contains( 'btn')) {  // проверка по имени класса
+        return
+    }
+    removeCardActiveClass();
+
+    // ищем ближайшего предка со имени класса - метод  closest('.имя класса')
+    const parentCard = e.target.closest('.colorItem');
+    addActiveClassParent(parentCard);
+    createBodyColor(e.target.dataset.color)
+ 
+}
+
+
+// убирает активный класс
+function removeCardActiveClass() {
+     // поиск текущий активный класс - формула
+const currentCardActive = document.querySelector('.colorList.is-active');
+    //ищем через уточьнение, так как на проэкте может быть много is-active
+if (currentCardActive) {
+        currentCardActive.classList.remove('is-active')
+    }
+};
+
+
+// добавляет активный клас
+function addActiveClassParent(card) {
+    card.classList.add('is-active')
+}
+
+// меняет цвет фона в зависимости от выбраного 
+function createBodyColor(color) {
+   document.body.style.backgroundColor = color
+}
---
+++ js/colors.js
@@ -0,0 +1,32 @@
+const colors = [
+    {
+        label: "yellow",
+        rgb: "#FFFF00"
+    },
+    {
+        label: "red",
+        rgb: "#FF3300"
+    },
+    {
+        label: "blue",
+        rgb: "#6600FF"
+    },
+    {
+        label: "orange",
+        rgb: "#FF9900"
+    },
+    {
+        label: "green",
+        rgb: "#00FF00"
+    },
+    {
+        label: "violet",
+        rgb: "#FF33FF"
+    },
+    {
+        label: "aise",
+        rgb: "#6600FF"
+    }
+];
+
+export default colors
